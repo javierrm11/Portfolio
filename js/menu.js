e8d7285e392ef0c8f68b24fd935bec889bc5d0b1
jose.gomez <javierrumo2@gmail.com>
2024-08-28 20:42:04 +0200

    desarrollo pagina inicial responsive

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('mostrar');
+const mn = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    // Obtener el estilo de visualización actual del menú
+    const displayStyle = window.getComputedStyle(mn).display;
+
+    // Alternar entre mostrar y ocultar el menú
+    if (displayStyle === 'none') {
+        mn.style.display = 'flex';
+    } else {
+        mn.style.display = 'none';
+    }
+});
+
